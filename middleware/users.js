150dae5943f907b7b3f4b5c34473dce5feb6130d
joaofreitas21 <64160079+joaofreitas21@users.noreply.github.com>
2022-11-27 22:33:41 +0000

    Updated

--- middleware/users.js
+++ middleware/users.js
@@ -1,47 +1,49 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = {
-    validateRegister: (req,res,next) => {
-        //username minimum length 4
-        if(!req.body.username || req.body.username.length < 4){
-            return res.status(400).send({
-                message: "Please enter a username with min. 4 chars",
-            });
-        }
-        // password min 6 characters
+  validateRegister: (req, res, next) => {
+    //username minimum length 4
+    if (!req.body.username || req.body.username.length < 4) {
+      return res.status(400).send({
+        message: "Please enter a username with min. 4 chars",
+      });
+    }
+    // password min 6 characters
 
-        if(!req.body.password || req.body.password.length < 6){
-            return res.status(400).send({
-                message: "Please enter a password with min. 6 chars",
-            });
-        }
-        // password (repeat) must match
-        if(!req.body.password_repeat || req.body.password != req.body.password_repeat){
-            return res.status(400).send({
-                message: "Both passwords must match",
-            });
-        }
-        next();
-    },
-    
-    isLoggedIn: (req,res,next) => {
-        if(!req.headers.authorization){
-            return res.status(400).send({
-                message: "Invalid Access!",
-            });
-        }
-        try{
-            // O Header apresenta a categoria, ex BearerToken + o token(dasdasd.dasdas.das)
-            const authHeader = req.headers.authorization;
-            const token = authHeader.split(' ')[1];
-            const decoded = jwt.verify(token, "SECRETKEY");
-            req.userData = decoded;
-            next();
-        } catch(err){
-            throw err;
-            return res.status(400).send({
-                message: "Your session is not valid!",
-            });
-        }
-    },
+    if (!req.body.password || req.body.password.length < 6) {
+      return res.status(400).send({
+        message: "Please enter a password with min. 6 chars",
+      });
+    }
+    // password (repeat) must match
+    if (
+      !req.body.password_repeat || req.body.password != req.body.password_repeat
+    ) {
+      return res.status(400).send({
+        message: "Both passwords must match",
+      });
+    }
+    next();
+  },
+
+  isLoggedIn: (req, res, next) => {
+    if (!req.headers.authorization) {
+      return res.status(400).send({
+        message: "You have no access to this section!",
+      });
+    }
+    try {
+      // O Header apresenta a categoria, ex BearerToken + o token(dasdasd.dasdas.das)
+      const authHeader = req.headers.authorization;
+      const token = authHeader.split(" ")[1];
+      const decoded = jwt.verify(token, "SECRETKEY");
+      req.userData = decoded;
+      next();
+    } catch (err) {
+      //throw err;
+      return res.status(400).send({
+        message: "Your session is not valid!",
+      });
+    }
+  },
 };
--- routes/router.js
+++ routes/router.js
@@ -1,111 +1,115 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const db = require('../database/db.js');
+const db = require("../database/db.js");
 const userMiddleware = require("../middleware/users.js");
-//const userMiddleware = require('../middleware/users.js');
-
 
 // localhost:3000/api/register
-router.post('/register', userMiddleware.validateRegister, (req,res,next) => {
-    db.query(`SELECT id FROM users WHERE LOWER(username) = LOWER(${req.body.username})`, (err,result) =>{
-        if(result){
-            return res.status(409).send({
-                message: "This username is already in use!",
+router.post("/register", userMiddleware.validateRegister, (req, res, next) => {
+  db.query(
+    `SELECT id FROM users WHERE LOWER(username) = LOWER(${req.body.username})`,
+    (err, result) => {
+      if (result) {
+        return res.status(409).send({
+          message: "This username is already in use!",
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            //throw err;
+            return res.status(500).send({
+              message: err,
             });
-        }
-        else{
-       
-            bcrypt.hash(req.body.password,10, (err,hash)=>{
-                if(err){
-                    //throw err;
-                    return res.status(500).send({
-                        message: err,
-                    });
-
+          } else {
+            db.query(
+              `INSERT INTO users (username,password,email,registered) VALUES 
+                        (${db.escape(req.body.username)},'${hash}',${db.escape(
+                req.body.email
+              )}, now());`,
+              (err, result) => {
+                if (err) {
+                  //throw err;
+                  return res.status(400).send({
+                    message: "Username or Email already in use",
+                  });
                 }
-                else{
-                    db.query(
-                        `INSERT INTO users (username,password,email,registered) VALUES 
-                        (${db.escape(req.body.username)},'${hash}',${db.escape(req.body.email)}, now());`,
-                        (err, result) => {
-                            if(err){
-                                //throw err;
-                                return res.status(400).send({
-                                    message: "Username or Email already in use",
-                                });
-                            }
-                            return res.status(201).send({
-                                message: "Registered successfully",
-                            });
-                        }
-                    );
-                    
-                }
-            });
-        }
-    })
+                return res.status(201).send({
+                  message: "Registered successfully",
+                });
+              }
+            );
+          }
+        });
+      }
+    }
+  );
 });
 
-
-
-
 // localhost:3000/api/login
-router.post('/login', (req,res,next) => {
-    db.query(`SELECT * FROM users WHERE username = ${db.escape(req.body.username)};`,
-    (err,result) => {
-        if(err){
-            //throw err;
-            return res.status(400).send({
-                message: err,
-            });
-        }
-
-        if(!result){
-            return res.status(403).send({
-                message: "Username or password not found",
-            });
-        }
-
-        bcrypt.compare(req.body.password,result[0]["password"],(bError,bResult)=>{
-            if(bError){
-                //throw bError;
-                return res.status(400).send({
-                    message: "Incorrect Password",
-                });
+router.post("/login", (req, res, next) => {
+  db.query(
+    `SELECT * FROM users WHERE username = ${db.escape(req.body.username)};`,
+    (err, result) => {
+      if (err) {
+        //throw err;
+        return res.status(400).send({
+          message: err,
+        });
+      }
+      //console.log(result);
+      if (!result.length) {
+        //console.log("ENTREI AQUI");
+        return res.status(403).send({
+          message: "Username not found",
+        });
+      } else {
+        bcrypt.compare(
+          req.body.password,
+          result[0]["password"],
+          (bError, bResult) => {
+            if (bError) {
+              //throw bError;
+              return res.status(400).send({
+                message: "Incorrect Password",
+              });
             }
-            if(bResult){
-                console.log(result[0].id);
-                const token = jwt.sign({
-                    username : result[0].username,
-                    userID: result[0].id,
-                },"SECRETKEY",
-                {expiresIn: "7d"}
-                );
-                db.query(`UPDATE users SET last_login = now() WHERE id = ${result[0].id};`);
-                return res.status(200).send({
-                    message: "Logged in",
-                    token,
-                    user: result[0],
-                });
+            if (bResult) {
+              console.log(result[0].id);
+              const token = jwt.sign(
+                {
+                  username: result[0].username,
+                  userID: result[0].id,
+                },
+                "SECRETKEY",
+                { expiresIn: "7d" }
+              );
+              db.query(
+                `UPDATE users SET last_login = now() WHERE id = ${result[0].id};`
+              );
+              result[0].password = '';
+              return res.status(200).send({
+                message: "Logged in",
+                token,
+                user: result[0],
+              });
             }
 
             return res.status(401).send({
-                message:"The password is incorrect",
+              message: "The password is incorrect",
             });
-        }
-
-    );
-    });
+          }
+        );
+      }
+    }
+  );
 });
 
-
 // localhost:3000/api/secretauth
-router.get('/secretauth', userMiddleware.isLoggedIn,(req,res,next) => {
-    console.log(req.userData);
-    res.send("If you can see it, it's because you have a verified JWToken!");
+router.get("/secretauth", userMiddleware.isLoggedIn, (req, res, next) => {
+  console.log(req.userData);
+  res.send("This is an autenticated only area!");
 });
 
 module.exports = router;
